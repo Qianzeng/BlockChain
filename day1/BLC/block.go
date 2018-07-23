bd7148c551bc038afda98f77ef65a7d764a86b32
qzeng <msnwinter115@hotmail.com>
2018-07-23 14:12:11 +0800

    Initial commit

---
+++ day1/BLC/block.go
@@ -0,0 +1,49 @@
+package BLC
+
+import (
+	"fmt"
+	"strconv"
+	"bytes"
+	"crypto/sha256"
+	"time"
+)
+type Block struct {
+	Height int64
+	PrevBlockHash []byte
+	Data []byte
+	Timestamp int64
+	Hash []byte
+}
+
+func (block *Block) SetHash() {
+	heightBytes := IntToHex(block.Height)
+	fmt.Println(heightBytes)
+	//2进制str
+	timeString := strconv.FormatInt(block.Timestamp, 2)
+	fmt.Println(timeString)
+	//2进制字节数组
+	timeBytes := []byte(timeString)
+	fmt.Println(timeBytes)
+	//拼接属性
+	blockBytes := bytes.Join([][]byte{
+		heightBytes,
+		block.PrevBlockHash,
+		block.Data,
+		timeBytes}, 
+		[]byte{})
+
+	hash := sha256.Sum256(blockBytes)
+	block.Hash = hash[:]
+}
+
+func NewBlock(data string,height int64,prevBlockHash []byte) *Block {
+
+	//创建区块
+	block := &Block{height,prevBlockHash,[]byte(data),time.Now().Unix(),nil}
+
+	//设置Hash
+	block.SetHash()
+
+	return block
+
+}
---
+++ day1/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+import "./BLC"
+
+func main() {
+	block := BLC.NewBlock("Genenis is Block", 1, []byte{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0})
+	fmt.Println(block)
+	fmt.Println("Qzeng")
+	fmt.Println("Qzeng")
+	fmt.Println("Qzeng")
+	fmt.Println("Qzeng")
+}
